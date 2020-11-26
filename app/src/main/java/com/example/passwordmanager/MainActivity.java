bc4295d6eaf18e4bd03c8c12d00c1157d22ba76d
Gerard <gerard.noguer@gmail.com>
2020-11-26 19:37:03 +0100

    Responsive Vault layout

--- app/src/main/java/com/example/passwordmanager/MainActivity.java
+++ app/src/main/java/com/example/passwordmanager/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.passwordmanager;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -21,6 +22,8 @@ public class MainActivity extends AppCompatActivity {
        logInButton.setOnClickListener(new View.OnClickListener(){
            public void onClick(View view){
 
+               Intent intent = new Intent(MainActivity.this, VaultActivity.class);
+               startActivity(intent);
 
            }
        });

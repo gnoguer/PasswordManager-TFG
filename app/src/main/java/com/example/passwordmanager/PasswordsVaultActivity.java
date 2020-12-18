58383bd55b9672bb202e1484a3eafe23185d3335
Gerard <gerard.noguer@gmail.com>
2020-12-18 17:35:12 +0100

    getItems from DB for passwordsVault

--- app/src/main/java/com/example/passwordmanager/PasswordsVaultActivity.java
+++ app/src/main/java/com/example/passwordmanager/PasswordsVaultActivity.java
@@ -1,17 +1,42 @@
 package com.example.passwordmanager;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.Toast;
 
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.security.GeneralSecurityException;
 import java.util.ArrayList;
+import java.util.Base64;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+
+import javax.crypto.SecretKey;
+import javax.crypto.SecretKeyFactory;
+import javax.crypto.spec.PBEKeySpec;
 
 public class PasswordsVaultActivity extends AppCompatActivity {
+
+    private ArrayList<Service> services = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,8 +49,13 @@ public class PasswordsVaultActivity extends AppCompatActivity {
 
         setSupportActionBar(toolbar);
 
-        ArrayList<Service> services = new ArrayList<>();
 
+
+        try {
+            getItems();
+        } catch (GeneralSecurityException | IOException e) {
+            e.printStackTrace();
+        }
 
         fab.setOnClickListener(new View.OnClickListener(){
 
@@ -34,5 +64,67 @@ public class PasswordsVaultActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+    }
+
+    public void getItems() throws GeneralSecurityException, IOException {
+
+        User user = SharedPrefManager.getInstance(getApplicationContext()).getUser();
+
+        StringRequest stringRequest = new StringRequest(Request.Method.GET, URLs.URL_GET_PASSWORDS + "&userId=" + user.getId(),
+                new Response.Listener<String>() {
+                    @RequiresApi(api = Build.VERSION_CODES.O)
+                    @Override
+                    public void onResponse(String response) {
+                        try {
+                            //converting response to json object
+                            JSONObject obj = new JSONObject(response);
+                            //if no error in response
+                            if (!obj.getBoolean("error")) {
+                                Toast.makeText(getApplicationContext(), obj.getString("message"), Toast.LENGTH_SHORT).show();
+
+                                //getting the user from the response
+
+                                JSONArray passwordsArray = obj.getJSONArray("passwords");
+
+                                for (int i = 0; i < passwordsArray.length(); i++){
+
+                                    JSONObject passwordJson = passwordsArray.getJSONObject(i);
+
+                                    String name = passwordJson.getString("name");
+                                    String username = passwordJson.getString("username");
+                                    String password = passwordJson.getString("password");
+                                    String note = passwordJson.getString("note");
+
+                                    services.add(new Service(name, username, password, note));
+                                }
+
+                                //starting the activity
+                                finish();
+
+                                startActivity(new Intent(getApplicationContext(), VaultActivity.class));
+
+                            } else {
+                                Toast.makeText(getApplicationContext(), obj.getString("message"), Toast.LENGTH_SHORT).show();
+                            }
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Toast.makeText(getApplicationContext(), error.getMessage(), Toast.LENGTH_SHORT).show();
+                    }
+                }) {
+            @Override
+            protected Map<String, String> getParams() throws AuthFailureError {
+                Map<String, String> params = new HashMap<>();
+                params.put("userId", String.valueOf(user.getId()));
+                return params;
+            }
+        };
+
+        VolleySingleton.getInstance(this).addToRequestQueue(stringRequest);
     }
 }
--- app/src/main/java/com/example/passwordmanager/URLs.java
+++ app/src/main/java/com/example/passwordmanager/URLs.java
@@ -5,7 +5,7 @@ public class URLs {
     private static final String ROOT_URL = "http://10.0.2.2/android/Api.php?apicall=";
 
     public static final String URL_REGISTER = ROOT_URL + "signup";
-    public static final String URL_LOGIN= ROOT_URL + "login";
-    public static final String URL_SAVE_PASSWORD= ROOT_URL + "savePass";
-
+    public static final String URL_LOGIN = ROOT_URL + "login";
+    public static final String URL_SAVE_PASSWORD = ROOT_URL + "savePass";
+    public static final String URL_GET_PASSWORDS = ROOT_URL + "getPasswords";
 }
